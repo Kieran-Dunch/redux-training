1e143734e81f143ac9139dbbdced832d3e5b9e25
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-14 11:33:49 -0700

    finish redux road

--- redux-road/index.js
+++ redux-road/index.js
@@ -14,7 +14,36 @@ const wagonReducer = (state = initialWagonState, action) => {
         supplies: state.supplies + 15,
         days: state.days + 1
       };
+    case 'travel':
+      if (state.supplies < 20) {
+        return state;
+      }
+      return {
+        ...state,
+        supplies: state.supplies - (20 * action.payload),
+        distance: state.distance + (10 * action.payload),
+        days: state.days + action.payload
+      };
+    case 'tippedWagon':
+      return {
+        ...state,
+        supplies: state.supplies - 30,
+        days: state.days + 1
+      };
     default:
       return state;
   }
 };
+
+
+// time to play!
+let wagon = wagonReducer(undefined, {});
+console.log(wagon); // { supplies: 100, distance: 0, days: 0 }
+wagon = wagonReducer(wagon, { type: 'travel', payload: 1 });
+console.log(wagon); // { supplies: 80, distance: 10, days: 1 }
+wagon = wagonReducer(wagon, { type: 'gather' });
+console.log(wagon); // { supplies: 95, distance: 10, days: 2 }
+wagon = wagonReducer(wagon, { type: 'tippedWagon' });
+console.log(wagon); // { supplies: 65, distance: 10, days: 3 }
+wagon = wagonReducer(wagon, { type: 'travel', payload: 3 });
+console.log(wagon); // { supplies: 5, distance: 40, days: 6 }
