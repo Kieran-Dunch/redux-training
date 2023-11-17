92382065f030d745301be2eeb6bb2c568b3838e2
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-16 17:07:41 -0700

    annotation

--- redux-api-intro/store.js
+++ redux-api-intro/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux'
 
 const initialState = 0;
+
+// the reducer is a function that takes in the current state and an action
+// the reducer returns the new state based on the action
+// the reducer is called whenever an action is dispatched
 const countReducer = (state = initialState, action) => {
   switch (action.type) {
     case "increment":
@@ -9,5 +13,9 @@ const countReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// creating a store to reinforce the one-way data flow of redux
+// the store is the single source of truth for the application
+// the store is created by passing in the reducer
 
 export const store = createStore(countReducer);
