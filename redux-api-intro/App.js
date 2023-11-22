09490b2258c9b3fef887c3c1f63532596361796b
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-22 13:57:07 -0700

    creating app.js

---
+++ redux-api-intro/App.js
@@ -0,0 +1,24 @@
+// using our redux store in a react application
+import React from 'react';
+import { increment, decrement } from './store';
+
+function App({ state, dispatch }) {
+  // dispatch incrememnt action
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  // dispatch decrement action
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  return (
+    <main>
+      <p id='counter'>waiting for current state</p>
+      <button id='incrementer' onClick={handleIncrement}>+</button>
+      <button id='decrementer' onClick={handleDecrement}>-</button>
+    </main>
+  )
+}
+
+export default App;
