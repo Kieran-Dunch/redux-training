0c61e9fb4f4e12c30be919c2b4540e73cf7083d0
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-28 07:23:40 -0700

    start search term action creators

--- strategies-for-complex-state/store.js
+++ strategies-for-complex-state/store.js
@@ -4,3 +4,16 @@ const initialState = {
   favoriteRecipes: [],
   searchTerm: ""
 }
+
+export const setSearchTerm = (term) => {
+  return {
+    type: 'searchTerm/setSearchTerm',
+    payload: term
+  };
+}
+
+export const clearSearchTerm = () => {
+  return {
+    type: 'searchTerm/clearSearchTerm'
+  };
+}
