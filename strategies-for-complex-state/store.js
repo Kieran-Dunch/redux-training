87e37c4adfb2611380d6f1f61e9b8e23819d64d8
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-28 07:33:21 -0700

    added reducer

--- strategies-for-complex-state/store.js
+++ strategies-for-complex-state/store.js
@@ -41,3 +41,36 @@ export const removeRecipe = (recipe) => {
     payload: recipe
   };
 }
+
+
+const recipesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'searchTerm/setSearchTerm':
+      return {
+        ...state,
+        searchTerm: action.payload
+      };
+    case 'searchTerm/clearSearchTerm':
+      return {
+        ...state,
+        searchTerm: ""
+      };
+    case 'allRecipes/loadData':
+      return {
+        ...state,
+        allRecipes: action.payload
+      };
+    case 'favoriteRecipes/addRecipe':
+      return {
+        ...state,
+        favoriteRecipes: [...state.favoriteRecipes, action.payload]
+      };
+    case 'favoriteRecipes/removeRecipe':
+      return {
+        ...state,
+        favoriteRecipes: state.favoriteRecipes.filter(recipe => recipe.id !== action.payload.id)
+      };
+    default:
+      return state;
+  }
+}
