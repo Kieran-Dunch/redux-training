b38f256776c7a81dd90544a644424ecb694f830f
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-28 07:28:19 -0700

    all recipe action creators

--- strategies-for-complex-state/store.js
+++ strategies-for-complex-state/store.js
@@ -1,3 +1,5 @@
+import allRecipesData from './data.js';
+
 // start with initial state
 const initialState = {
   allRecipes: [],
@@ -5,6 +7,7 @@ const initialState = {
   searchTerm: ""
 }
 
+// action creators
 export const setSearchTerm = (term) => {
   return {
     type: 'searchTerm/setSearchTerm',
@@ -15,5 +18,26 @@ export const setSearchTerm = (term) => {
 export const clearSearchTerm = () => {
   return {
     type: 'searchTerm/clearSearchTerm'
+  };
+}
+
+export const loadData = () => {
+  return {
+    type: 'allRecipes/loadData',
+    payload: allRecipesData
+  };
+}
+
+export const addRecipe = (recipe) => {
+  return {
+    type: 'favoriteRecipes/addRecipe',
+    payload: recipe
+  };
+}
+
+export const removeRecipe = (recipe) => {
+  return {
+    type: 'favoriteRecipes/removeRecipe',
+    payload: recipe
   };
 }
