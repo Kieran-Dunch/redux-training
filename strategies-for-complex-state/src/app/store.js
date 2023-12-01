cf1accd4012121385560fee85bc6c35af10dfebf
Kieran Dunch <dunch.kieran@gmail.com>
2023-12-01 07:54:24 -0700

    added slices

--- strategies-for-complex-state/src/app/store.js
+++ strategies-for-complex-state/src/app/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux';
-import allRecipesData from './data.js';
+import allRecipesData from '../data.js';
 
 // Action Creators
 ////////////////////////////////////////
