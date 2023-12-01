ee537d3a80dc04021cdf101b2d198b381ff15cf1
Kieran Dunch <dunch.kieran@gmail.com>
2023-12-01 07:59:47 -0700

    completed redux state management

--- strategies-for-complex-state/src/app/store.js
+++ strategies-for-complex-state/src/app/store.js
@@ -1,83 +1,8 @@
 import { createStore, combineReducers } from 'redux';
-import allRecipesData from '../data.js';
 
-// Action Creators
-////////////////////////////////////////
-
-export const addRecipe = (recipe) => {
-  return {
-    type: 'favoriteRecipes/addRecipe',
-    payload: recipe
-  };
-}
-
-export const removeRecipe = (recipe) => {
-  return {
-    type: 'favoriteRecipes/removeRecipe',
-    payload: recipe
-  };
-}
-
-export const setSearchTerm = (term) => {
-  return {
-    type: 'searchTerm/setSearchTerm',
-    payload: term
-  }
-}
-
-export const clearSearchTerm = () => {
-  return {
-    type: 'searchTerm/clearSearchTerm'
-  };
-}
-
-export const loadData = () => {
-  return {
-    type: 'allRecipes/loadData',
-    payload: allRecipesData
-  };
-}
-
-// Reducers
-////////////////////////////////////////
-// rewriting the reducer to only address one piece of state at a time
-// also creating slice-specific initial state
-
-
-const initialAllRecipes = [];
-const allRecipesReducer = (allRecipes = initialAllRecipes, action) => {
-  switch (action.type) {
-    case 'allRecipes/loadData':
-      return action.payload
-    default:
-      return allRecipes;
-  }
-}
-
-const initialSearchTerm = '';
-const searchTermReducer = (searchTerm = initialSearchTerm, action) => {
-  switch (action.type) {
-    case 'searchTerm/setSearchTerm':
-      return action.payload;
-    case 'searchTerm/clearSearchTerm':
-      return '';
-    default:
-      return searchTerm;
-  }
-}
-
-// Create the initial state for this reducer.
-const initialFavoriteRecipes = [];
-const favoriteRecipesReducer = (favoriteRecipes = initialFavoriteRecipes, action) => {
-  switch (action.type) {
-    case 'favoriteRecipes/addRecipe':
-      return [...favoriteRecipes, action.payload];
-    case 'favoriteRecipes/removeRecipe':
-      return favoriteRecipes.filter(recipe => recipe.id !== action.payload.id);
-    default:
-      return favoriteRecipes;
-  }
-}
+import { allRecipesReducer } from '../features/allRecipes/allRecipesSlice';
+import { searchTermReducer } from '../features/searchTerm/searchTermSlice';
+import { favoriteRecipesReducer } from '../features/favoriteRecipes/favoriteRecipesSlice';
 
 // use combineReducers to create a single reducer from multiple reducers
 
