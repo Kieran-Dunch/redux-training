dddc6667cf9f9045b03a61c04b4460533be60c65
Kieran Dunch <dunch.kieran@gmail.com>
2023-12-08 08:46:52 -0500

    search term

--- codecademy-store/src/app/App.js
+++ codecademy-store/src/app/App.js
@@ -5,12 +5,15 @@ import { CurrencyFilter } from '../features/currencyFilter/CurrencyFilter.js';
 // Import the Cart component here.
 import { Cart } from '../features/cart/Cart.js';
 // Render the Cart component below <Inventory />
+import { SearchTerm } from '../features/searchTerm/SearchTerm.js';
+
 export const App = (props) => {
 
   const { state, dispatch } = props;
 
   return (
     <div>
+      <SearchTerm searchTerm={state.searchTerm} dispatch={dispatch} />
       <CurrencyFilter
         currencyFilter={state.currencyFilter}
         dispatch={dispatch}
--- codecademy-store/src/features/searchTerm/searchTermSlice.js
+++ codecademy-store/src/features/searchTerm/searchTermSlice.js
@@ -4,14 +4,43 @@ Extra Credit:
     * 'searchTerm/setSearchTerm'
     * 'searchTerm/clearSearchTerm'
     * Don't forget to set the initial state and return state by default!
+*/
+export const searchTermReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'searchTerm/setSearchTerm': {
+      return action.payload;
+    }
+    case 'searchTerm/clearSearchTerm': {
+      return '';
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
+/*
 2. Create a function called setSearchTerm
     * It has one parameter, term
     * It returns an action object whose payload is the term value
     * See SearchTerm.js for how this will be used.
 
+*/
+export const setSearchTerm = (term) => {
+  return {
+    type: 'searchTerm/setSearchTerm',
+    payload: term
+  };
+};
+
+/*
 3. Create a function called clearSearchTerm
     * It returns an action object with no payload
     * See SearchTerm.js for how this will be used.
-
 */
+
+export const clearSearchTerm = () => {
+  return {
+    type: 'searchTerm/clearSearchTerm'
+  };
+}
