89aab179018d6f57e2723cb10fe2ba7d6a622012
Kieran Dunch <dunch.kieran@gmail.com>
2024-01-03 08:44:59 -0700

    create async thunk

---
+++ redux-middleware-and-thunks/allRecipesSlice.js
@@ -0,0 +1,25 @@
+import { fetchRecipes } from '../../app/api'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+
+const loadRecipes = createAsyncThunk('allRecipes/loadRecipes', async () => {
+  const data = await fetchRecipes();
+  const json = await data.json();
+  return json;
+});
+
+export const allRecipesSlice = createSlice({
+  name: 'allRecipes',
+  initialState: {
+    recipes: [],
+    isLoading: false,
+    hasError: false,
+  },
+  reducers: {
+    addRecipes(state, action) {
+      state.recipes = action.payload
+    }
+  },
+});
+
+export default allRecipesSlice.reducer;
