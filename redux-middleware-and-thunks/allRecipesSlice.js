77ca0403963855235d86163137c3bcf60ff3d182
Kieran Dunch <dunch.kieran@gmail.com>
2024-01-04 11:38:06 -0700

    added annotations

--- redux-middleware-and-thunks/allRecipesSlice.js
+++ redux-middleware-and-thunks/allRecipesSlice.js
@@ -2,11 +2,19 @@ import { fetchRecipes } from '../../app/api'
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 
-const loadRecipes = createAsyncThunk('allRecipes/loadRecipes', async () => {
-  const data = await fetchRecipes();
-  const json = await data.json();
-  return json;
-});
+const loadRecipes = createAsyncThunk(
+  'allRecipes/loadRecipes',
+  async () => {
+    const data = await fetchRecipes();
+    const json = await data.json();
+    return json;
+  }
+);
+
+// The above call to createAsyncThunk will generate what three action types?
+// 1. allRecipes/loadRecipes/pending
+// 2. allRecipes/loadRecipes/fulfilled
+// 3. allRecipes/loadRecipes/rejected
 
 export const allRecipesSlice = createSlice({
   name: 'allRecipes',
