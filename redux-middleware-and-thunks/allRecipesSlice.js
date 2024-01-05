d393f9eb252abd62bc535116bab58295ecac9f67
Kieran Dunch <dunch.kieran@gmail.com>
2024-01-05 08:02:33 -0700

    extra reducers property

--- redux-middleware-and-thunks/allRecipesSlice.js
+++ redux-middleware-and-thunks/allRecipesSlice.js
@@ -16,6 +16,15 @@ const loadRecipes = createAsyncThunk(
 // 2. allRecipes/loadRecipes/fulfilled
 // 3. allRecipes/loadRecipes/rejected
 
+// createSlice will generate the action creators and action types for the
+// reducers defined in the reducers object. The action types will be prefixed
+// with the name of the slice, in this case allRecipes. So the action type
+// for the addRecipes reducer will be allRecipes/addRecipes.
+
+// the problem is that the createSlice can't handle the action types generated
+// by createAsyncThunk. So we need to use the extraReducers object to handle
+// those action types.
+
 export const allRecipesSlice = createSlice({
   name: 'allRecipes',
   initialState: {
@@ -28,6 +37,21 @@ export const allRecipesSlice = createSlice({
       state.recipes = action.payload
     }
   },
+  extraReducers: {
+    [loadRecipes.pending]: (state, action) => {
+      state.isLoading = true;
+      state.hasError = false;
+    },
+    [loadRecipes.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.hasError = false;
+      state.recipes = action.payload;
+    },
+    [loadRecipes.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.hasError = true;
+    }
+  }
 });
 
 export default allRecipesSlice.reducer;
